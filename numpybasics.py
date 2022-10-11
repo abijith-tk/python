06a35f9e81e6f73660d2b158b342ae14d3c54f54
abijith-tk <abijithtk1212@gmail.com>
2022-10-11 10:49:21 +0530

    dtype,itemsize,array()

--- numpybasics.py
+++ numpybasics.py
@@ -13,4 +13,22 @@ print(a.shape)
 
 # size is used to find the total number of elements in the array.
 # that is product of the shape
-print(a.size)
+print(a.size)  
+
+
+# dtype is used to find the data type of the array
+print(a.dtype)
+
+# itemsize is used to find the bytes of each element in the array
+print(a.itemsize)
+
+# array() is used to create an array using python tuple or list
+# syntax, array(*tuple/list*,dtype=*data type*)
+b=array([1,2,3,4])
+print(b)
+
+# creating multi-dimensional arrays using array()
+
+c=array([[1,2],[2,3]])
+print(c)
+
