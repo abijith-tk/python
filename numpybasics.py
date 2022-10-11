f83384720f0e7e862d284f922bbbfd7196736183
abijith-tk <abijithtk1212@gmail.com>
2022-10-11 11:09:37 +0530

    zeros,ones,empty

--- numpybasics.py
+++ numpybasics.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 # numpy basics
-
+# arange() is used to add a sequence of numbers to an array
+# syntax, arange(start,stop,step)   * start,update are optional arguments
 # reshape() is used to specify the shape of the matrix
 a=np.arange(10).reshape(5,2)
 
@@ -24,11 +25,22 @@ print(a.itemsize)
 
 # array() is used to create an array using python tuple or list
 # syntax, array(*tuple/list*,dtype=*data type*)
-b=array([1,2,3,4])
+b=np.array([1,2,3,4])
 print(b)
 
 # creating multi-dimensional arrays using array()
 
-c=array([[1,2],[2,3]])
+c=np.array([[1,2],[2,3]])
 print(c)
+
+# zeros() is used to create an array of zeroes
+# ones() is used to create an array of ones
+# empty() is used to create an empty array array whose initial content is random and depends on the state of the memory.
+ 
+c=np.zeros((3,4))
+d=np.ones((2,3))
+e=np.empty((2,2))
+print(c)
+print(d)
+print(e)
 
